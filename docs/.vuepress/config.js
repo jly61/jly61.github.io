4a190687fb2a556068cfbb359779aa3493ef2ad9
jly <896503709@qq.com>
2021-10-12 17:33:27 +0800

    '测试自动部署'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "lll's blog",
-  description: 'web前端技术博客。',
+  description: 'web前端技术博客111。',
   // base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
