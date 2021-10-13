54c1e3b1b65e80638c896f49cec6c9336163e1f9
jly <896503709@qq.com>
2021-10-13 10:48:40 +0800

    '部署'

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ fi
 git init
 git add -A
 git commit -m "部署"
-git push -f githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f ${githubUrl} master:gh-pages # 推送到github gh-pages分支
 
 cd -
 rm -rf docs/.vuepress/dist
